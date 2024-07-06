1812dc55d264ff9e25cf721160c205d9bef8bcc8
林泽凯 <1342210664@qq.com>
2024-07-06 23:59:50 +0800

    feat: 新增回到顶部

---
+++ components/BackToTop.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    data-aos-offset="100"
+    data-aos="fade-up"
+    class="fixed bottom-4 right-4 cursor-pointer"
+    @click="scrollToTop"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-8 h-8"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M4.5 15.75l7.5-7.5 7.5 7.5"
+      />
+    </svg>
+  </div>
+</template>
+
+<script setup>
+import { nextTick } from 'vue';
+
+function scrollToTop() {
+  nextTick(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+}
+</script>

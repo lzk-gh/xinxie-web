a5083b9da44c9d9dab0e47b927517f2f5c54b50a
林泽凯 <1342210664@qq.com>
2024-07-07 20:29:36 +0800

    feat: 新增 layout 布局

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="body">
+    <Header />
+    <main>
+      <slot></slot>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup lang='ts'>
+</script>
+
+<style scoped></style>

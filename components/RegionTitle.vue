a07d7702676c0fe08b45cc5dcb36527887f7b6ea
林泽凯 <1342210664@qq.com>
2024-07-07 20:36:22 +0800

    feat: 封装首页盒子标题组件

---
+++ components/RegionTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="font-semibold" :class="align">
+    <h3
+      data-aos-easing="ease-out"
+      data-aos="fade-up"
+      data-aos-duration="1000"
+      class="obvious-text-color text-xl"
+    >
+      <slot name="tag-title" />
+    </h3>
+    <div
+      data-aos="fade-down"
+      data-aos-easing="ease-out"
+      data-aos-duration="800"
+      class="text-3xl font-semibold"
+    >
+      <slot name="title" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  align: 'text-center' | 'text-left' | 'text-right';
+}>();
+</script>

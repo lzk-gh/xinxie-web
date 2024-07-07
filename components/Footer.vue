c379c99e64504a4f1d137c9f83514261391b22fb
林泽凯 <1342210664@qq.com>
2024-07-07 20:26:30 +0800

    feat: 新增页脚组件

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="bg-gray-800 text-white py-4 px-4 md:px-8 text-center">
+    <div class="flex justify-center space-x-4 text-sm">
+      <p>@2024 版权所有 信息科技协会</p>
+      <a
+        href="https://beian.miit.gov.cn/"
+        target="_blank"
+        class="text-gray-400 hover:text-gray-300"
+      >
+        <span>粤ICP备XXXXXXXX号</span>
+      </a>
+    </div>
+  </footer>
+</template>

7c9d4a309e99ba4ec9d07bd0874cb7bc87e46551
林泽凯 <1342210664@qq.com>
2024-07-07 20:40:04 +0800

    feat: 新增首页

---
+++ pages/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <HomeCover />
+  <HomeIntro />
+  <HomeCadre />
+  <HomeJoinUs />
+  <BackToTop />
+</template>
+
+<script setup lang="ts">
+import HomeCover from './HomeCover.vue';
+import HomeIntro from './HomeIntro.vue';
+import HomeCadre from './HomeCadre.vue';
+import HomeJoinUs from './HomeJoinUs.vue';
+</script>

f88263868b9d3714abaf479eb1a1ad05155f26b7
林泽凯 <1342210664@qq.com>
2024-07-07 20:21:41 +0800

    fix: 修改 AOS 的配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+
 export default defineNuxtConfig({
-  css: ['~/assets/styles/theme.css'],
+  css: ['~/assets/styles/theme.css', 'aos/dist/aos.css'],
   modules: [
+    'nuxtjs-naive-ui',
     '@nuxtjs/tailwindcss',
-    'nuxt-aos',
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt'
   ],
   devtools: { enabled: true },
   devServer: {
     port: 4000
+  },
+  vite: {
+    ssr: {
+      noExternal: ['naive-ui', '@css-render/vue3-ssr', 'vueuc', 'date-fns']
+    },
+    plugins: [
+      { src: '~/plugins/aos.client.ts', mode: 'client' },
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar'
+            ]
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ]
   }
-  // pinia: {
-  //   storesDirs: ['~/stores/**', '#/stores/**', '@/stores/**']
-  // }
 });

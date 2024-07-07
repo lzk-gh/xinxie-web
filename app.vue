df8a72dfc8735d61f100762d9029f7892fd01c5b
林泽凯 <1342210664@qq.com>
2024-07-07 20:19:22 +0800

    fix: 修复刷新界面样式冲突，禁用了naive的 preflight

--- app.vue
+++ app.vue
@@ -1,29 +1,11 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <NConfigProvider preflight-style-disabled>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </NConfigProvider>
 </template>
 
 <style>
-/*:root {*/
-/*  --theme-main-bg-color: #f5f5f9;*/
-/*  --theme-header-bg-color: #ffffff;*/
-/*  --theme-border-color: #e8e8ec;*/
-/*}*/
-
-/*.dark {*/
-/*  --theme-main-bg-color: #1b1f25;*/
-/*  --theme-header-bg-color: #171a20;*/
-/*  --theme-border-color: #2c2f34;*/
-
-/*  background-color: #171a20;*/
-/*}*/
-
-/*::view-transition-group(root) {*/
-/*  animation: none;*/
-/*}*/
-
-/*.dark::view-transition-old(root) {*/
-/*  z-index: 10;*/
-/*}*/
+/**/
 </style>
